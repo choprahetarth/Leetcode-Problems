b85fc8fbe2598c66e9f6d7459483e1f5b98b3c16
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2022-06-07 23:13:28 +0530

    Time: 60 ms (95.09%), Space: 15.2 MB (49.08%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {} # value : index
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in hash_table:
+                return ([hash_table[diff], i ])
+            hash_table[n] = i
+                
+                
+                
